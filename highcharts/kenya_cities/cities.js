697739752fed9e04a62e60e6280a4e50e334f1cc
nsrivast <nsrivast@gmail.com>
2015-06-22 23:48:06 +0300

    initial course files

---
+++ highcharts/kenya_cities/cities.js
@@ -0,0 +1,30 @@
+var cityData = [
+  { name: "Nakuru", color: "#1b9e77", y: 307990}, 
+  { name: "Naivasha", color: "#1b9e77", y: 181966}, 
+  { name: "Molo", color: "#1b9e77", y: 107806}, 
+  { name: "Kisumu", color: "#d95f02", y: 409928}, 
+  { name: "Awasi", color: "#d95f02", y: 93369}, 
+  { name: "Ahero", color: "#d95f02", y: 76828}, 
+  { name: "Kangundo-Tala", color: "#7570b3", y: 218557}, 
+  { name: "Machakos", color: "#7570b3", y: 150041}, 
+  { name: "Athi River", color: "#7570b3", y: 139380}, 
+  { name: "Ruiru", color: "#e7298a", y: 238858}, 
+  { name: "Kikuyu", color: "#e7298a", y: 233231}, 
+  { name: "Thika", color: "#e7298a", y: 139853}, 
+  { name: "Karuri", color: "#e7298a", y: 129934}, 
+  { name: "Limuru", color: "#e7298a", y: 104282}, 
+  { name: "Kiambu", color: "#e7298a", y: 88869}, 
+  { name: "Kitui", color: "#66a61e", y: 155896}, 
+  { name: "Mwingi", color: "#66a61e", y: 83803}, 
+  { name: "Kitale", color: "#e6ab02", y: 106187}, 
+  { name: "Nandi Hills", color: "#e6ab02", y: 73626}
+];
+
+var countyData = [
+  { name: "Nakuru", color: "#1b9e77", x: 10, y: 100 },
+  { name: "Kisumu", color: "#d95f02", x: 10, y: 180 },
+  { name: "Machakos", color: "#7570b3", x: 10, y: 260 },
+  { name: "Kiambu", color: "#e7298a", x: 10, y: 340 },
+  { name: "Kitui", color: "#66a61e", x: 10, y: 495 },
+  { name: "Trans-Nzoia", color: "#e6ab02", x: 10, y: 545 }
+];
