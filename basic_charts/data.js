d9e2b8b2be0cc5ee8c08141f67a0877178d0a65a
nsrivast <nsrivast@gmail.com>
2015-07-08 01:29:14 +0300

    lec3

---
+++ basic_charts/data.js
@@ -0,0 +1,14 @@
+var sampleData = [
+
+  { name: 'alpha', q1: 3, q2: 55, q3: 9, c1: 'T', c2: 'Male', o1: 'Low'}, 
+  { name: 'beta', q1: 3, q2: 50, q3: 10, c1: 'F', c2: 'Male', o1: 'Hi'}, 
+  { name: 'gamma', q1: 1, q2: 58, q3: 9, c1: 'F', c2: 'Male', o1: 'Med'}, 
+  { name: 'delta', q1: 4, q2: 55, q3: 10, c1: 'T', c2: 'Female', o1: 'Med'}, 
+  { name: 'epsilon', q1: 7, q2: 56, q3: 4, c1: 'F', c2: 'Female', o1: 'Low'}, 
+  { name: 'zeta', q1: 6, q2: 50, q3: 7, c1: 'F', c2: 'Female', o1: 'Low'}, 
+  { name: 'eta', q1: 10, q2: 57, q3: 6, c1: 'F', c2: 'Female', o1: 'Low'}, 
+  { name: 'theta', q1: 1, q2: 56, q3: 10, c1: 'F', c2: 'Female', o1: 'Low'}, 
+  { name: 'iota', q1: 10, q2: 57, q3: 5, c1: 'F', c2: 'Female', o1: 'Low'}, 
+  { name: 'kappa', q1: 8, q2: 54, q3: 10, c1: 'F', c2: 'Female', o1: 'Low'}, 
+
+];
